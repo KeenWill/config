214a90bde53cf91cefe32dee354088db456f540a
William Goeller <william@williamgoeller.com>
2025-01-19 13:25:31 -0500

    nit

--- machines/wkg-server0/hardware-configuration.nix
+++ machines/wkg-server0/hardware-configuration.nix
@@ -21,7 +21,7 @@
   fileSystems."/boot" =
     { device = "/dev/disk/by-uuid/D53C-73F7";
       fsType = "vfat";
-      options = [ "fmask=0022" "dmask=0022" ];
+      options = [ "fmask=0077" "dmask=0077" "defaults" ];
     };
 
   swapDevices = [ ];
