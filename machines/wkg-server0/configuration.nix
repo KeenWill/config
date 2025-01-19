65781baa293f8b2529bf97c17869359b95e30340
William Goeller <william@williamgoeller.com>
2025-01-19 16:52:34 -0500

    _

--- machines/wkg-server0/configuration.nix
+++ machines/wkg-server0/configuration.nix
@@ -51,10 +51,15 @@
   virtualisation.oci-containers = {
     backend = "podman";
     containers.homeassistant = {
+
+      autoStart = true;
       ports = [ "8123:8123" ];
       volumes = [ "home-assistant:/config" ];
       environment.TZ = "America/New_York";
       image = "ghcr.io/home-assistant/home-assistant:2025.1";
+        privateNetwork = true;
+        hostBridge = "br0";
+	useDHCP = true;
       extraOptions = [ 
 	"--cap-add=CAP_NET_RAW,CAP_NET_BIND_SERVICE"
         # "--device=/dev/ttyACM0:/dev/ttyACM0"  # Example, change this to match your own hardware
