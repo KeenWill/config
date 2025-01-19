7fbeaf6df160db819511fa99825e5e609bc042ae
William Goeller <william@williamgoeller.com>
2025-01-19 17:18:43 -0500

    _

--- machines/wkg-server0/containers/home-assistant.nix
+++ machines/wkg-server0/containers/home-assistant.nix
@@ -3,9 +3,9 @@
       ports = [ "8123:8123" ];
       volumes = [ "home-assistant:/config" ];
       environment.TZ = "America/New_York";
-      pull = "always";
       image = "ghcr.io/home-assistant/home-assistant:stable";
       extraOptions = [ 
+	"pull=newer"
 	"--cap-add=CAP_NET_RAW,CAP_NET_BIND_SERVICE"
         # "--device=/dev/ttyACM0:/dev/ttyACM0"  # Example, change this to match your own hardware
       ];
