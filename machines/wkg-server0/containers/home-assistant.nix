1cbaf269f05a3f52c87c6925a64c2a4680db6f8c
William Goeller <william@williamgoeller.com>
2025-01-19 17:37:30 -0500

    _

--- machines/wkg-server0/containers/home-assistant.nix
+++ machines/wkg-server0/containers/home-assistant.nix
@@ -5,7 +5,7 @@
       environment.TZ = "America/New_York";
       image = "ghcr.io/home-assistant/home-assistant:stable";
       extraOptions = [ 
-	"pull=newer"
+	"--pull=newer"
 	"--cap-add=CAP_NET_RAW,CAP_NET_BIND_SERVICE"
         # "--device=/dev/ttyACM0:/dev/ttyACM0"  # Example, change this to match your own hardware
       ];
